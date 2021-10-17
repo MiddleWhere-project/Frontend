749d11c04377ad3b9ae024c558588930a2de5cec
soihs821 <hyunsung9999@gmail.com>
2021-10-17 17:38:21 +0900

    #1 중간지점찾기 수정, 추가 및 삭제버튼 구현, 그 외 자잘한 부분 수정

--- home.js
+++ home.js
@@ -1,13 +1,46 @@
-const addButton = document.querySelector('#add-button');
+// html 코드에 script 부분 넣어놔서 이 파일 필요 없을듯 해요
 
+// insertRow() 는 행(tr)추가, insertCell()은 열(td) 추가
+function rowAdd() {
+  var objRow;
+  objRow = document.all("tblShow").insertRow();
 
-function addToList() {
-  const list = document.querySelector('#list');
+  /*
+  var objCell = objRow.insertCell();
+  objCell.innerHTML =
+    "<li class='item-list-user'> <table> <tr> <td> <img src='img/user.png' alt='사용자 아이콘' /> </td><td id='start_location'> <p> <input type='text' placeholder='탭하여 출발 위치를 입력해주세요'/></p><hr /></td></tr></table></li>";
+*/
 
-  const newListItem = document.createElement('li');
-  newListItem.classList.add('item-list-user'); 
+  // 사용자 아이콘
+  var objCell_Icon = objRow.insertCell();
+  objCell_Icon.innerHTML = " <img src='img/user.png' alt='사용자 아이콘'/>";
 
-  newListItem.innerHTML = //li안에 있는 table 태그를 넣고 싶은데,,;
+  // 출발 위치 입력창
+  var objCell_Start = objRow.insertCell();
+  objCell_Start.innerHTML =
+    "<input type='text' placeholder='탭하여 출발 위치를 입력해주세요' id='start_location'/>";
 
-  list.appendChild(newListItem);
+  // 삭제 버튼
+  var objCell_Del = objRow.insertCell();
+  objCell_Del.innerHTML =
+    "<button type='button' id='del-button'> <img src='img/del.png' alt='del_btn' /> </button>";
+}
+
+function addRow() {
+  var objRow;
+  objRow = document.all("locations").insertRow();
+
+  // 새 행(Row) 추가
+  //const newRow = table.insertRow();
+
+  // 새 행에 Cell 추가
+  var newCell1 = objRow.insertCell();
+  var newCell2 = objRow.insertCell();
+
+  // Cell에 내용 추가
+  newCell1.innerHTML("<td><img src='img/user.png' alt='사용자 아이콘' /></td>");
+
+  newCell2.innerHTML(
+    "<td><input type='text' placeholder='탭하여 출발 위치를 입력해주세요' id='start_location' /><hr /></td>"
+  );
 }
